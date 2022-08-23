3a9242d3cad37b9a1b15c8fee3a198bfd5d106b3
edwu1 <edmundzjwu@gmail.com>
2022-08-23 12:25:26 -0400

    Added more Shufflez info, and more tarot info

--- minting-dapp/src/scripts/react/Info.tsx
+++ minting-dapp/src/scripts/react/Info.tsx
@@ -19,36 +19,53 @@ const Info = () => {
 
         <p>
           <h2>About Us</h2>
-          Shufflez is a newly established project from Montreal, Canada. For the foreseeable future, we plan to bring divination and tarot cards to the web3 space in the form of NFTs. 
+          Shufflez is a newly established project from <strong className="text-highlight">Montreal, Canada</strong>. For the foreseeable future, we plan to bring divination and tarot cards to the web3 space in the form of NFTs. 
           To accelerate our project, give us a follow on Twitter and join our Discord. 
         </p>
 
         <p className="socials-info">
-        <span className="emoji">🌹 </span><strong><a href='https://www.instagram.com/shufflez.nft/' target='_blank'>Instagram</a></strong>, for NFT previews. <br/>
-          Join the community on <strong><a href='https://discord.gg/9xbSmaecKw' target='_blank'>Discord</a></strong><span className="emoji"> 🔮</span>! <br/>
-          If you like us, give us a follow on <strong><a href='https://twitter.com/ShufflezNft' target='_blank'>Twitter</a></strong><span className="emoji"> ✨</span>! <br/>
-          We are also available on <strong><a href='https://www.tiktok.com/@shufflez.nft' target='_blank'>Tiktok</a></strong><span className="emoji"> ❤️‍🔥</span>. <br/>
+        <span className="emoji">💦 </span><strong><a href='https://www.instagram.com/shufflez.nft/' target='_blank'>Instagram</a></strong>, for NFT previews. <br/>
+          Learn more about tarots on our <strong><a href='https://discord.gg/9xbSmaecKw' target='_blank'>Discord</a></strong>!<span className="emoji"> 🔮</span> <br/>
+          Join the community on <strong><a href='https://twitter.com/ShufflezNft' target='_blank'>Twitter</a></strong>.<span className="emoji"> ✨</span> <br/>
+          We are also available on <strong><a href='https://www.tiktok.com/@shufflez.nft' target='_blank'>Tiktok</a></strong>. <span className="emoji"> ❤️‍🔥</span> <br/>
+        </p>
+
+        <p>
+          <h2>Project Plan</h2>
+          We want to cater a <strong className="text-highlight">fun</strong> community where artists, developers, and diviners alike 
+          can share their passion for <strong className="text-highlight">gems</strong>, <strong className="text-highlight">astrology</strong>, 
+          and <strong className="text-highlight">tarot cards</strong>, in the form of <strong className="text-highlight">NFTs</strong>! <br/>
+          We'd like to build a common space for divination and web3 enthusiasts to <strong className="text-highlight">discuss</strong> 
+          and <strong className="text-highlight">share ideas</strong> for tarot decks.
+        </p>
+
+        <p>
+          Once accomplished, we ambitiously hope to put the NFTs to use in the metaverse. <strong className="text-highlight">Imagine tarot reading in the metaverse</strong>! 💎
         </p>
 
         <p>
           <h2>Why should NFT communities be exclusive?</h2>
-          Shufflez is all about inclusivity 🙌! You don't have to own a Shufflez NFT to join the community. Instead, we will encourage you 
+          Shufflez is all about <strong className="text-highlight">inclusivity</strong>! 🙌 You don't have to own a Shufflez NFT to join the community. Instead, we will encourage you 
           to create your own tarot-themed NFT! 
         </p>
 
         <p>
-          <strong className="italic text-center content-center">"Then why should you own a Shufflez NFT?"</strong> <br/>
-          We want to cater a fun community where artists, developers, and diviners alike 
-          can share their passion for gems, astrology, and tarot cards, in the form of NFTs!
+          <strong className="italic text-center content-center">"Then why should I own a Shufflez NFT?"</strong>
         </p>
 
         <p>
-          You will be able to sell and buy your own neat tarot-themed NFT to an already existing audience, and
-          by owning a Shufflez NFT, you will contribute in keeping the project running, and accelerating its development. 
-          We are eternally grateful for your support!
+          Collect a full-on 78 card deck and we will send you a <strong className="text-highlight">printed copy</strong> in <strong className="text-highlight">real life</strong>! 😱 <br/>
+
+          In addition, join the community to buy and sell exclusive, hand-drawn, tarot-themed NFTs! The community will be already 
+          established.
         </p>
 
-        <h2>FAQ <span className="emoji">🤔</span></h2>
+        <p>  
+          <strong className="text-highlight">We are truly grateful for your support. Even just for reading our website</strong>. 🙏
+          By owning a Shufflez NFT, you will contribute in keeping the project running, accelerating its development, and providing joy in the niche! 
+        </p>
+
+        <h2>FAQ and Info <span className="emoji">🤔</span></h2>
         <Questions />
       </div>
     </>
--- minting-dapp/src/scripts/react/Questions.tsx
+++ minting-dapp/src/scripts/react/Questions.tsx
@@ -15,16 +15,69 @@ export default function Questions() {
         activeIndex={activeIndex}
         setActiveIndex={setActiveIndex}
       >
-        In the simplest terms, tarot cards are used for fortune-telling to gain insight on one's situation. 
-        Each card usually has an underlying meaning, traditionally depicted by the symbolisms on the card's illustration. 
-        Tarot cards have a nearly 600 year history. 
-        The most known deck is the Rider tarot deck comprised of 78 cards, with 22 <strong>trumps</strong> and 56 <strong>suit cards</strong>. 
+        In the simplest terms, tarot cards are used for <strong className="text-highlight">fortune-telling</strong> to gain insight on one's situation. 
+        Each card usually has an underlying meaning, traditionally depicted by the <strong className="text-highlight">symbolisms</strong> on the card's illustration. 
+        Tarot cards have a nearly <strong className="text-highlight">600 year history</strong>. <br/>
+
+        The most known deck is the Rider tarot deck comprised of 78 cards, with 22 <strong className="text-highlight">trumps</strong> and 56 <strong className="text-highlight">suit cards</strong>. 
         Many variations exist today. See <a href='https://www.britannica.com/topic/tarot' target='_blank'> detailed explanation <div className="icons text-sm"><FaExternalLinkAlt /></div></a>.
       </AccordionLayout>
 
       <AccordionLayout 
-        title="How is this collection organized?"
+        title="🌹 The story of tarot decks. 🌹"
         index={2}
+        activeIndex={activeIndex}
+        setActiveIndex={setActiveIndex}
+      >
+        Many tarot decks today <strong className="text-highlight">depict meaning</strong> that seemingly come from our gut feeling. We associate plants, animals, astrology, 
+        planets, gems, jewelry, alchemy, and so on, to something dear to our minds. Some may say that this is a typical <strong className="text-highlight">human behavior</strong>, 
+        that we do in order to <strong className="text-highlight">connect with nature</strong>, and even, <strong className="text-highlight">perhaps with God</strong>. 
+        Others may say that this is a <strong className="text-highlight">means to an end</strong>. We do this to <strong className="text-highlight">heal</strong> our souls, for comfort and for peace of mind. 
+        Whatever it is, both may be right. These are definitions of art 
+        after all. <br/><br/>
+        
+        But tarot cards go a step further. They depict <strong className="text-highlight">symbols</strong> that, 
+        we may <strong className="text-highlight">never have seen before</strong>, yet we still understand! 
+        This is what  <strong className="text-highlight">Carl Jung</strong> called the <strong className="text-highlight">collective subconscious</strong>. 
+        Civilizations across the globe, and civilizations across different eras, all worshipped the Sun. <br/><br/>
+        
+        I can still clearly remember the day a teacher brought The Magician in class and asked us to analyze the symbolisms. 
+        We have never seen the card before, yet collectively, we gave out every single symbolism there was to say. <br/><br/>
+
+        Even if tarot decks originally started in Italy as playing cards, they quickly were adopted for fortune-telling in France 
+        because of their rich symbolisms.
+      </AccordionLayout>
+
+      <AccordionLayout 
+        title="🥀 The story of the Rider deck. 🥀"
+        index={3}
+        activeIndex={activeIndex}
+        setActiveIndex={setActiveIndex}
+      >
+        Surprisingly, the Rider tarot deck is extremely well-thought-out. It has an uncountable amount of symbolisms (which 
+        our NFT deck pales in comparison 😅), but we will give an introduction here. <br/><br/>
+
+        First and foremost, each card depicts <strong className="text-highlight">symbolisms</strong> that is most always understood (apart from the 6 of Swords). On top of this, 
+        whether the creator intentionally wanted this, each card can be associated with a <strong className="text-highlight">zodiac sign</strong>, a 
+        <strong className="text-highlight">ruling planet</strong>, and an <strong className="text-highlight"> alchemical element</strong>.
+        This is typical of more modern decks. <br/><br/>
+
+        The genius I think, comes from the <strong className="text-highlight">numerology</strong>. The intricate numerology relates cards together. For example, The Tower (numbered 16) 
+        is related to the omnipotent Magician (numbered 1) and the Lovers (numbered 6). The number 6 signifies a journey to harmony. This meaning fits 
+        with the underlying meaning of the cards! <br/><br/>
+
+        In addition to this, the trumps are well arranged in a <strong className="text-highlight">cycle</strong>, each card being where it needs to be. 
+        At the start of our journey, we are a Fool (Card 0), candide as ever. With each trump comes a timely life lesson. 
+        For example, before properly facing the Devil (Card 15), you must face all others before it. 
+        At the end of our journey, we face Judgement (Card 20) and the World renews (Card 21). <br/><br/>
+
+        This is in fact, just an introduction. Feel free to join the community to learn more 😜!
+
+      </AccordionLayout>
+
+      <AccordionLayout 
+        title="How is this collection organized?"
+        index={4}
         activeIndex={activeIndex}
         setActiveIndex={setActiveIndex}
       >
@@ -34,22 +87,11 @@ export default function Questions() {
 
       <AccordionLayout 
         title="What are the NFT dimensions?"
-        index={3}
+        index={5}
         activeIndex={activeIndex}
         setActiveIndex={setActiveIndex}
       >
         Our collection is in PNG format 2560px by 2560px, all hand-drawn. Time and effort was put into them <span className="emoji">😤</span>!
-      </AccordionLayout>
-
-      <AccordionLayout 
-        title="What about future plans?"
-        index={4}
-        activeIndex={activeIndex}
-        setActiveIndex={setActiveIndex}
-      >
-        We plan to release variations of tarot decks! Contact us if you'd like to put your deck out! We'd like to build a common 
-        space for divination and web3 enthusiasts, and your art would be greatly appreciated <span className="emoji">😝</span>! 
-        The idea is to give free guidance in a community setting, for you to release your own fun, hand-drawn deck. 
       </AccordionLayout>
 
     </div>
